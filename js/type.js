049cbd6d832d4acbcf8a52aec8064ab6cb59b45b
Arnab <arnab4srk@gmail.com>
2020-05-12 17:58:52 +0530

    ;______;

---
+++ js/type.js
@@ -0,0 +1,21 @@
+const Skills = ["B.Tech Under Grad", "Front-End Web Developer", "Java Programmer", "Student"];
+let index =0;
+let skillIndex = 0;
+let skill = "";
+( function type(){
+    if(skillIndex === Skills.length){
+        skillIndex = 0;
+    }
+    skill = Skills[skillIndex];
+    let char = skill.slice(0, ++index);
+    console.log(char);
+    document.querySelector(".type-text").textContent = char;
+    if(char.length === skill.length){
+        skillIndex++;
+        index = 0;
+        setTimeout(type, 1000);
+    }
+    else{
+        setTimeout(type, 200);
+    }
+}());
