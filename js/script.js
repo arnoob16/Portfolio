4a6cf1553acad8825bcd08d95d27782f780a796b
Arnab <arnab4srk@gmail.com>
2020-05-19 18:03:23 +0530

    Modal Added

--- js/script.js
+++ js/script.js
@@ -31,7 +31,7 @@ var Project = {
         description: "LearnYuva is an Ed-Tech Startup which aims to provide Education with the help of Live & Interactive sessions.<br> LearnYuva is an online educational platform which focuses primarily on educating those aspiring students who wish to take a step forward and do more than the necessary by not just achieving their targets in school, but also in the outside world.<br>The Front-End of the Official site of our Startup was developed by me and <a href='https://www.linkedin.com/in/anurag-singh-07723719b/'>Anurag</a>. Anurag also designed the UI of the website. The Backend work was done by <a href='https://www.linkedin.com/in/rahmansaqib/'>Saqib</a>.",
         link : "https://learnyuva.live/",
         repo: "https://github.com/anuragbyte/ly",
-        image: "../Assets/Portfolio/LearnYuva.png",
+        image: "/Assets/Portfolio/LearnYuva.png",
         category: "Website",
         date: "May, 2020."
     },
@@ -40,7 +40,7 @@ var Project = {
         description: "LearnYuva is an Ed-Tech Startup which aims to provide Education with the help of Live & Interactive sessions.<br> LearnYuva is an online educational platform which focuses primarily on educating those aspiring students who wish to take a step forward and do more than the necessary by not just achieving their targets in school, but also in the outside world.<br>The Front-End of the Official site of our Startup was developed by me and <a href=''>Anurag</a>. Anurag also designed the UI of the website. The Backend work was done by <a href=''>Saqib</a>.",
         link : "https://arnab4srk.github.io/Real-Madrid/",
         repo: "https://github.com/arnab4srk/RealMadrid",
-        image: "../Assets/Portfolio/RealMadrid.png",
+        image: "/Assets/Portfolio/RealMadrid.png",
         category: "Website",
         date: "May, 2020."
     },
@@ -49,7 +49,7 @@ var Project = {
         description: "LearnYuva is an Ed-Tech Startup which aims to provide Education with the help of Live & Interactive sessions.<br> LearnYuva is an online educational platform which focuses primarily on educating those aspiring students who wish to take a step forward and do more than the necessary by not just achieving their targets in school, but also in the outside world.<br>The Front-End of the Official site of our Startup was developed by me and <a href=''>Anurag</a>. Anurag also designed the UI of the website. The Backend work was done by <a href=''>Saqib</a>.",
         link : "https://arnab4srk.github.io/Portfolio/",
         repo: "https://github.com/arnab4srk/Portfolio",
-        image: "../Assets/Portfolio/Portfolio.png",
+        image: "/Assets/Portfolio/Portfolio.png",
         category: "Website",
         date: "May, 2020."
     }
